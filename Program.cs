d2884f036ad7ac7d4541baf4e4a8e9238967b5a4
AgoProDev <ago.pro.dev@gmail.com>
2026-03-05 12:37:00 +0100

    initial code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// validate parameters
+if (args.Length != 2 || !int.TryParse(args[1], out int votes))
+{
+    Console.WriteLine("Usage: dotnet run <city> <votes>");
+    return;
+}
+var city = args[0];
+Console.WriteLine($"Find outlet for {city} with minimal {votes} votes.");
+
+string outlet = findOutlet(city, votes);
+
+Console.WriteLine($"Finest outlet for {city} is {outlet}.");
+
+string findOutlet(string city, int voutes)
+{
+  return "<TODO>";
+}
