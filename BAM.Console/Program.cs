ed28e64192608838c89908c316545a460b792807
AgoProDev <ago.pro.dev@gmail.com>
2026-03-07 18:01:08 +0100

    BAM.ExcerciseSolutions library - code for different now as classes and can be run from BAM.Console specyfing <solution> <city> <votes>

--- BAM.Console/Program.cs
+++ BAM.Console/Program.cs
@@ -1,17 +1,34 @@
-﻿// validate parameters
-if (args.Length != 2 || !int.TryParse(args[1], out int votes))
+﻿using BAM.ExcerciseSolutions;
+using System.Text.RegularExpressions;
+
+// validate parameters
+if (args.Length != 3 || !Regex.IsMatch(args[0], "^(s1|p[1-3])$") || !int.TryParse(args[2], out int votes))
 {
-    Console.WriteLine("Usage: dotnet run <city> <votes>");
-    return;
+  Console.WriteLine("Usage: dotnet run <solution> <city> <votes>");
+  Console.WriteLine("Solutions:");
+  Console.WriteLine("\ts1 - sequential #1");
+  Console.WriteLine("\tp1 - parallel #1");
+  Console.WriteLine("\tp2 - parallel #2 (with thread local state)");
+  Console.WriteLine("\tp3 - parallel #3 (with thread local state & UTF8JsonReader)");
+  return;
 }
-var city = args[0];
+var solution = args[0];
+var city = args[1];
 Console.WriteLine($"Find outlet for {city} with minimal {votes} votes.");
 
-string outlet = findOutlet(city, votes);
-
-Console.WriteLine($"Finest outlet for {city} is {outlet}.");
-
-string findOutlet(string city, int voutes)
+string outlet = solution switch
 {
-  return "<TODO>";
+  "s1" => new Sequential_1().findOutlet(city, votes),
+  "p1" => new Parallel_1().findOutlet(city, votes),
+  "p2" => new Parallel_2().findOutlet(city, votes),
+  "p3" => new Parallel_3().findOutlet(city, votes),
+  _ => throw new ArgumentException($"Unknown solution: {solution}")
+};
+if (string.IsNullOrEmpty(outlet))
+{
+  Console.WriteLine($"No outlet found for {city} with minimal {votes} votes.");
+}
+else
+{
+  Console.WriteLine($"Finest outlet for {city} is `{outlet}`.");
 }
--- BAM.ExcerciseSolutions/Parallel_2_local_thread_state/Parallel_2.cs
+++ BAM.ExcerciseSolutions/Parallel_2_local_thread_state/Parallel_2.cs
@@ -1,100 +1,90 @@
 ﻿/*
  * based on commit #4e9cb74@main
  */
-
 // validate parameters
 using System.Net.Http.Json;
 using System.Text.Json;
 
-if (args.Length != 2 || !int.TryParse(args[1], out int votes))
+namespace BAM.ExcerciseSolutions;
+
+public class Parallel_2
 {
-  Console.WriteLine("Usage: dotnet run <city> <votes>");
-  return;
-}
-var city = args[0];
-
-Console.WriteLine($"Find outlet for {city} with minimal {votes} votes.");
-
-var httpClient = new HttpClient();
-var jsonSerializerOptions = new JsonSerializerOptions
-{
-  PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
-};
-
-string outlet = findOutlet(city, votes);
-
-Console.WriteLine($"Finest outlet for {city} is {outlet}.");
-
-string findOutlet(string city, int voutes)
-{
-  var urlFormat = $"https://jsonmock.hackerrank.com/api/food_outlets?city={city}&page={{0}}";
-  ResultsDto? resultDto = httpClient.GetFromJsonAsync<ResultsDto>(string.Format(urlFormat, 1), jsonSerializerOptions).Result;
-  if (resultDto is null || resultDto.Total == 0)
+  readonly HttpClient httpClient = new HttpClient();
+  readonly JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions
   {
-    return ""; // not found
-  }
-  var allFinest = findFinestOutletWithMinimalVotes(resultDto.Data, votes);
-  var are = new AutoResetEvent(true);
-  Parallel.For<OutletDto?>(2, resultDto.TotalPages + 1, 
-    //new ParallelOptions {  MaxDegreeOfParallelism = 2}, // this was set for debuging to limit to 2 threads, but in general let runtime to decide the optimal number
-    () => null, (page, _, threadFinest) =>  {
-    //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Staring Page {page} with threadFinest: {threadFinest}.");
-    ResultsDto? batchResult = httpClient.GetFromJsonAsync<ResultsDto>(string.Format(urlFormat, page), jsonSerializerOptions).Result;
-    OutletDto? batchFinest = findFinestOutletWithMinimalVotes(resultDto.Data, votes);
-    if (threadFinest is null)
+    PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+  };
+  public string findOutlet(string city, int votes)
+  {
+    var urlFormat = $"https://jsonmock.hackerrank.com/api/food_outlets?city={city}&page={{0}}";
+    ResultsDto? resultDto = httpClient.GetFromJsonAsync<ResultsDto>(string.Format(urlFormat, 1), jsonSerializerOptions).Result;
+    if (resultDto is null || resultDto.Total == 0)
     {
-      //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning batchFinset: {batchFinest} as no threadFinest");
-      return batchFinest;
+      return ""; // not found
     }
-    if (batchFinest is not null && batchFinest.UserRating.AverageRating > threadFinest.UserRating.AverageRating)
-    {
-      //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning batchFinset: {batchFinest} as better");
-      return batchFinest;
-    }
-    //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning threadFinest: {threadFinest} as better than batchFinnest");
-    return threadFinest;
-  }, 
-  (threadFinest) =>  {
-    if (threadFinest is not null)
-    {
-      are.WaitOne();
-      //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Compare finest {finest} with threadFinest {threadFinest}");
-      if (threadFinest is not null && threadFinest.UserRating.AverageRating > allFinest.UserRating.AverageRating)
+    var allFinest = findFinestOutletWithMinimalVotes(resultDto.Data, votes);
+    var are = new AutoResetEvent(true);
+    Parallel.For<OutletDto?>(2, resultDto.TotalPages + 1,
+      //new ParallelOptions {  MaxDegreeOfParallelism = 2}, // this was set for debuging to limit to 2 threads, but in general let runtime to decide the optimal number
+      () => null, (page, _, threadFinest) => {
+        //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Staring Page {page} with threadFinest: {threadFinest}.");
+        ResultsDto? batchResult = httpClient.GetFromJsonAsync<ResultsDto>(string.Format(urlFormat, page), jsonSerializerOptions).Result;
+        OutletDto? batchFinest = findFinestOutletWithMinimalVotes(resultDto.Data, votes);
+        if (threadFinest is null)
+        {
+          //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning batchFinset: {batchFinest} as no threadFinest");
+          return batchFinest;
+        }
+        if (batchFinest is not null && batchFinest.UserRating.AverageRating > threadFinest.UserRating.AverageRating)
+        {
+          //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning batchFinset: {batchFinest} as better");
+          return batchFinest;
+        }
+        //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Page {page} returning threadFinest: {threadFinest} as better than batchFinnest");
+        return threadFinest;
+      },
+    (threadFinest) => {
+      if (threadFinest is not null)
       {
-        //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] ThreadFinest {threadFinest} is better than {finest}");
-        allFinest = threadFinest;
+        are.WaitOne();
+        //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Compare finest {finest} with threadFinest {threadFinest}");
+        if (threadFinest is not null && threadFinest.UserRating.AverageRating > allFinest.UserRating.AverageRating)
+        {
+          //Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] ThreadFinest {threadFinest} is better than {finest}");
+          allFinest = threadFinest;
+        }
+        are.Set();
       }
-      are.Set();
-    }
-  });
-  Console.WriteLine($"Finest outlet is {allFinest}.");
-  return allFinest?.Name;
-}
-OutletDto? findFinestOutletWithMinimalVotes(List<OutletDto> outlets, int votes)
-{
-  return outlets.AsParallel().Where(o => o.UserRating.Votes >= votes).AsSequential().OrderByDescending(o => o.UserRating.AverageRating).FirstOrDefault();
-}
-
-public class ResultsDto
-{
-  public int Page { get; set;  }
-  public int PerPage { get; set;  }
-  public int Total { get; set;  }
-  public int TotalPages { get; set;  }
-  public List<OutletDto> Data { get; set; }
-}
-public class OutletDto
-{
-  public string City { get;set; }
-  public string Name { get;set; }
-  public UserRatingDto UserRating  { get;set; }
-  public override string ToString()
-  {
-    return $"Outlet {Name} rating {UserRating.AverageRating} and votes {UserRating.Votes}";
+    });
+    Console.WriteLine($"Finest outlet is {allFinest}.");
+    return allFinest?.Name;
   }
-}
-public class UserRatingDto
-{
-  public decimal AverageRating { get;set; }
-  public int Votes { get;set; }
+
+  OutletDto? findFinestOutletWithMinimalVotes(List<OutletDto> outlets, int votes)
+  {
+    return outlets.AsParallel().Where(o => o.UserRating.Votes >= votes).AsSequential().OrderByDescending(o => o.UserRating.AverageRating).FirstOrDefault();
+  }
+  class ResultsDto
+  {
+    public int Page { get; set;  }
+    public int PerPage { get; set;  }
+    public int Total { get; set;  }
+    public int TotalPages { get; set;  }
+    public List<OutletDto> Data { get; set; }
+  }
+  class OutletDto
+  {
+    public string City { get;set; }
+    public string Name { get;set; }
+    public UserRatingDto UserRating  { get;set; }
+    public override string ToString()
+    {
+      return $"Outlet {Name} rating {UserRating.AverageRating} and votes {UserRating.Votes}";
+    }
+  }
+  class UserRatingDto
+  {
+    public decimal AverageRating { get;set; }
+    public int Votes { get;set; }
+  }
 }
